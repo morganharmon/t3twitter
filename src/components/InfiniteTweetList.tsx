ebeb1e81669247d439aff6bf2fe99bbc540707ce
Morgan Harmon <morganharmon@gmail.com>
2023-06-25 18:00:55 -0700

    1:29 but infinite scroll broken

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -3,6 +3,7 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import { ProfileImage } from "./ProfileImage";
 import { VscHeart, VscHeartFilled } from "react-icons/vsc";
 import { useSession } from "next-auth/react";
+import { IconHoverEffect } from "./IconHoverEffect";
 
 type Tweet = {
     id: string;
@@ -86,9 +87,21 @@ function HeartButton({ likedByMe, likeCount }: HeartButtonProps) {
             <span>{likeCount}</span>
         </div>)
     }
-    return (
-    <div className="mb-1 mt-1 flex items-center gap-3 self-start text-gray-500">
-    <HeartIcon />
-    <span>{likeCount}</span>
-</div>)
+    return ( 
+        <button 
+        className={`group -ml-2 items-center gap-1 self-start flex transition-colors duration-200 ${
+            likedByMe 
+                ? "text-red-500" 
+                : "text-gray-500 hover:text-red-500 focus-visible:text-red-500"
+        }`}
+        >
+            <IconHoverEffect red>
+            <HeartIcon className={`transition-colors duration-200 ${
+                likedByMe 
+                ? "fill-red-500" 
+                : "fill-gray-500 group-hover:fill-red-500 group-focus-visible:fill-red-500"}`}
+            />
+            </IconHoverEffect>
+            <span>{likeCount}</span>
+        </button>)
 }
